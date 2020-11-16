6cf0e2ff61f06364b54d6379e900b9eff5b67122
59013-78-13 <59013-78-13@exchange.ad.afpanet>
2020-11-16 11:06:50 +0100

    first commit

---
+++ app/App.js
@@ -0,0 +1,29 @@
+class App 
+{
+    static start() 
+    {
+        window.onpopstate = () => 
+        {
+            App.browse();
+        }
+
+        $('.nav-link').on('click', (evt) => 
+        {
+            let btn = $(evt.target).closest('.navbar').find('.navbar-toggler').not('.collapsed');
+            btn ? btn.click() : null;
+        });
+
+        $('document').on('ready')
+        {
+            App.browse();
+        }
+    }
+
+    static browse()
+    {
+        console.clear();
+        let hash = (window.location.hash || "#accueil").substring(1);
+        $('main').hide().html(hash).fadeIn();
+
+    }
+}
